c91b1043604d03863cc4ff6eb58ddcab05760d41
aliplizal <145531495+aliplizal@users.noreply.github.com>
2023-11-16 22:37:09 +0700

    01 & 03

--- Soal01Dan03.java
+++ Soal01Dan03.java
@@ -1,8 +1,5 @@
-import java.util.Scanner;
-
 public class Soal01Dan03 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         int i;
         int total = 0;
 
